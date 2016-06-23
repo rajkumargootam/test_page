59f263e768ae8cb7e7dfca56172cd43a54d01ae6
rajkumar <rajkumar.gootam@gmail.com>
2016-06-23 19:23:25 +0530

    Added row and delete row

---
+++ app.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  console.log("Inside");
+  var counter = 0;
+
+
+  $("#add").click(function(){
+    if(counter >= 5){
+          alert("Only 5 boxes allowed");
+          return false;
+        }
+    else{
+      ++counter;
+      var ele = $(document.createElement('div')).attr("id", 'box' + counter).attr("class", 'box').html("Added row "+ counter);
+      ele.appendTo("#main");
+    }
+    console.log(counter);
+  });
+
+<div id="box1" class="box">Added row 1</div>
+
+  $("#del").click(function(){
+    if(counter <= 0){
+          alert("No more boxes to delete");
+          return false;
+        }
+    else{
+      var eleId = "#box" + counter;
+      console.log(eleId);
+      $(eleId).remove();
+      counter--;
+    }
+  });
+
+});
